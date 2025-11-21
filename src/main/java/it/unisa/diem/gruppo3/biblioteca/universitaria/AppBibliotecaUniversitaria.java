dce027252ba1cd2c641f28dc4298c0ce6f37cc73
francesco-vecchione <>
2025-11-21 09:53:55 +0100

    Third Commit: Progetto ricreato con una classe MainProxy perchè la build precedente dava problemi in fase di esecuzione

---
+++ src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/AppBibliotecaUniversitaria.java
@@ -0,0 +1,24 @@
+package it.unisa.diem.gruppo3.biblioteca.universitaria;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX AppBibliotecaUniversitaria
+ */
+public class AppBibliotecaUniversitaria extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/MainProxy.java
@@ -0,0 +1,7 @@
+package it.unisa.diem.gruppo3.biblioteca.universitaria;
+
+public class MainProxy {
+    public static void main(String[] args) {
+        AppBibliotecaUniversitaria.main(args);
+    }
+}
