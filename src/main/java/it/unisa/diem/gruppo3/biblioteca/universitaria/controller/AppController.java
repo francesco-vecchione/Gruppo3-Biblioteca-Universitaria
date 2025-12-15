5f688ea2f1903a32079f02a23385672cffe7c3c9
giovanni-scelzo <>
2025-12-15 11:56:28 +0100

    implementazione cancella, cerca, eliminafiltri finestra Libri

--- src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/controller/AppController.java
+++ src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/controller/AppController.java
@@ -11,102 +11,108 @@ import it.unisa.diem.gruppo3.biblioteca.universitaria.view.LibriDialog;
 import it.unisa.diem.gruppo3.biblioteca.universitaria.view.LoginDialog;
 import it.unisa.diem.gruppo3.biblioteca.universitaria.view.UtentiDialog;
 import it.unisa.diem.gruppo3.biblioteca.universitaria.view.ViewBiblioteca;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import javafx.collections.transformation.FilteredList;
 import javafx.stage.Stage;
 
 /**
  * @author gruppo 3
  */
-
-
 /**
-    * @brief Astrae il concetto di controller del pattern MVC, e l'unico servizio che offre in maniera
-    * pubblica è quello di essere istanziata. Ha la responsabilità di passare i dati dalla view al model e viceversa
-*/
+ * @brief Astrae il concetto di controller del pattern MVC, e l'unico servizio
+ * che offre in maniera pubblica è quello di essere istanziata. Ha la
+ * responsabilità di passare i dati dalla view al model e viceversa
+ */
 public class AppController {
-     
+
     /**
-    * @brief Il model che astrae il concetto di archivio libri
-    */
+     * @brief Il model che astrae il concetto di archivio libri
+     */
     private /*final*/ ModelArchivio<Libro> modelLibri;
-    
+
     /**
-    * @brief Il model che astrae il concetto di archivio utenti
-    */
+     * @brief Il model che astrae il concetto di archivio utenti
+     */
     private /*final*/ ModelArchivio<Utente> modelUtenti;
-    
+
     /**
-    * @brief Il model che astrae il concetto di archivio prestiti
-    */
+     * @brief Il model che astrae il concetto di archivio prestiti
+     */
     private /*final*/ ModelArchivio<Prestito> modelPrestiti;
-    
+
     /**
-    * @brief Il model che astrae il contenitore della password
-    */
+     * @brief Il model che astrae il contenitore della password
+     */
     private /*final*/ ModelPassword modelPassword;
-    
+
     /**
-    * @brief La view che astrae la finestra principale della pagine dove sono presenti le tab
-    * degli archivi dei libri, degli utenti e dei prestiti
-    */
+     * @brief La view che astrae la finestra principale della pagine dove sono
+     * presenti le tab degli archivi dei libri, degli utenti e dei prestiti
+     */
     private /*final*/ ViewBiblioteca viewBiblioteca;
     /**
-    * @brief La view che astrae la finestra di pop up generica generabile dal premere uno dei
-    * bottoni della tab dei libri
-    */ 
+     * @brief La view che astrae la finestra di pop up generica generabile dal
+     * premere uno dei bottoni della tab dei libri
+     */
     private /*final*/ LibriDialog popUpLibri;
     /**
-    * @brief La view che astrae la finestra di pop up generica generabile dal premere uno dei
-    * bottoni della tab degli utenti
-    */ 
+     * @brief La view che astrae la finestra di pop up generica generabile dal
+     * premere uno dei bottoni della tab degli utenti
+     */
     private /*final*/ UtentiDialog popUpUtenti;
     /**
-    * @brief La view che astrae la finestra di pop up generica generabile dal premere uno dei
-    * bottoni della tab dei prestiti
-    */ 
+     * @brief La view che astrae la finestra di pop up generica generabile dal
+     * premere uno dei bottoni della tab dei prestiti
+     */
     private /*final*/ UtentiDialog popUpPrestiti;
     /**
-    * @brief La view che astrae la finestra di log in visualizzata prima dell'apertura della
-    * finestra principale
-    */ 
+     * @brief La view che astrae la finestra di log in visualizzata prima
+     * dell'apertura della finestra principale
+     */
     private /*final*/ LoginDialog stageLogIn;
     /**
-    * @brief La view che astrae la finestra di sign up e la finestra di recupero password; le
-    * due differenziano solo per il titolo della finestra e un'etichetta descrittiva
-    */
+     * @brief La view che astrae la finestra di sign up e la finestra di
+     * recupero password; le due differenziano solo per il titolo della finestra
+     * e un'etichetta descrittiva
+     */
     private /*final*/ CreazionePasswordDialog stageCreazionePassword;
-    
+
     /**
-    * @brief È il costruttore di AppController, che accetta una reference di tipo Stage come parametro
-    * 
-    * @param[in] Stage si occupa di istanziare gli attributi di AppController, di inizializzare
-    *                  gli event handlers e i bindings
-    */
+     * @brief È il costruttore di AppController, che accetta una reference di
+     * tipo Stage come parametro
+     *
+     * @param[in] Stage si occupa di istanziare gli attributi di AppController,
+     * di inizializzare gli event handlers e i bindings
+     */
     public AppController(Stage stage) {
         modelLibri = new ModelArchivio<>("libri");
         modelUtenti = new ModelArchivio<>("utenti");
         modelPrestiti = new ModelArchivio<>("prestiti");
-        
+
         modelLibri.apriArchivio();
         modelUtenti.apriArchivio();
         modelPrestiti.apriArchivio();
-        
+
         viewBiblioteca = new ViewBiblioteca(stage, modelLibri.getArchivioFiltrato(), modelUtenti.getArchivioFiltrato(), modelPrestiti.getArchivioFiltrato());
-        
+
         inizializzaEventHandlers();
     }
-    
+
     /**
-    * @brief Inizializza gli event handlers
-    */
+     * @brief Inizializza gli event handlers
+     */
     private void inizializzaEventHandlers() {
         inizializzaEventHandlersLibri();
         inizializzaEventHandlersPrestiti();
         inizializzaEventHandlersUtenti();
     }
-    
+
     /**
-    * @brief Inizializza gli event handlers che riguardano Libri
-    */
+     * @brief Inizializza gli event handlers che riguardano Libri
+     */
     private void inizializzaEventHandlersLibri() {
         viewBiblioteca.getTabLibri().getBtnAggiungi().setOnAction(event -> {
             LibriDialog dialog = new LibriDialog();
@@ -119,58 +125,75 @@ public class AppController {
             Libro nuovo = dialog.getLibro();
             modelLibri.modificaElemento(target, nuovo);
         });
-        viewBiblioteca.getTabLibri().getBtnCancella().setOnAction(event -> new ConfermaAlert("Vuoi davvero eliminare " + viewBiblioteca.getTabLibri().getSelectedItem().toString()));  //a cui passo l'oggetto
+        viewBiblioteca.getTabLibri().getBtnCancella().setOnAction(event -> {
+            Libro target = viewBiblioteca.getTabLibri().getSelectedItem();
+            ConfermaAlert alert = new ConfermaAlert("Vuoi davvero eliminare " + target.toString());
+            if (alert.getEsito()) {
+                modelLibri.rimuoviElemento(target);
+            }
+        });
         viewBiblioteca.getTabLibri().getBtnCerca().setOnAction(event -> {
-            
+            String testoInserito = viewBiblioteca.getTabLibri().getTxfFiltroRicerca().getText();
+            String[] filtri = testoInserito.split(" ");
+            List<Predicate<Libro>> filtriPredicate = new ArrayList<>();
+            for (String filtro : filtri) {
+                filtriPredicate.add(libro -> libro.toString().toLowerCase().contains(filtro.toLowerCase()));
+            }
+            FilteredList<Libro> cercati = modelLibri.getArchivioFiltrato().filtered(
+                    filtriPredicate.stream()
+                            .reduce(Predicate::and)
+                            .orElse(x -> true) //se non ci sonon filtri resituisce tutto
+            );
+            viewBiblioteca.getTabLibri().getTabella().setItems(cercati);
         });
         viewBiblioteca.getTabLibri().getBtnEliminaFiltri().setOnAction(event -> {
             viewBiblioteca.getTabLibri().getTxfFiltroRicerca().clear();
-            //ripristino della tabella
+            viewBiblioteca.getTabLibri().getTabella().setItems(modelLibri.getArchivioFiltrato());
         });
     }
-    
+
     /**
-    * @brief Inizializza gli event handlers che riguardano Utenti
-    */
+     * @brief Inizializza gli event handlers che riguardano Utenti
+     */
     private void inizializzaEventHandlersUtenti() {
         viewBiblioteca.getTabUtenti().getBtnAggiungi().setOnAction(event -> new UtentiDialog());
         viewBiblioteca.getTabUtenti().getBtnModifica().setOnAction(event -> new UtentiDialog());  //a cui passo l'oggetto
         viewBiblioteca.getTabUtenti().getBtnCancella().setOnAction(event -> new UtentiDialog());  //a cui passo l'oggetto
         viewBiblioteca.getTabUtenti().getBtnCerca().setOnAction(event -> {
-            
+
         });
         viewBiblioteca.getTabUtenti().getBtnEliminaFiltri().setOnAction(event -> {
             viewBiblioteca.getTabUtenti().getTxfFiltroRicerca().clear();
             //ripristino della tabella
         });
     }
-    
+
     /**
-    * @brief Inizializza gli event handlers che riguardano Prestiti
-    */
+     * @brief Inizializza gli event handlers che riguardano Prestiti
+     */
     private void inizializzaEventHandlersPrestiti() {
         //viewBiblioteca.getTabPrestiti().getBtnAggiungi().setOnAction(event -> new PrestitiDialog());
         //viewBiblioteca.getTabPrestiti().getBtnModifica().setOnAction(event -> new PrestitiDialog());  //a cui passo l'oggetto
         viewBiblioteca.getTabPrestiti().getBtnCerca().setOnAction(event -> {
-            
+
         });
         viewBiblioteca.getTabPrestiti().getBtnEliminaFiltri().setOnAction(event -> {
             viewBiblioteca.getTabPrestiti().getTxfFiltroRicerca().clear();
             //ripristino della tabella
         });
     }
-    
+
     /**
-    * @brief Inizializza gli event handlers che riguardano Passwrod
-    */
+     * @brief Inizializza gli event handlers che riguardano Passwrod
+     */
     private void inizializzaEventHandlersPassword() {
-        
+
     }
-    
+
     /**
-    * @brief Inizializza i bindings
-    */
+     * @brief Inizializza i bindings
+     */
     private void inizializzaBindings() {
-        
+
     }
 }
--- src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/view/ConfermaAlert.java
+++ src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/view/ConfermaAlert.java
@@ -6,15 +6,28 @@
 package it.unisa.diem.gruppo3.biblioteca.universitaria.view;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 /**
  *
  * @author giosc
  */
 public class ConfermaAlert {
+    
+    private boolean esito = false;
 
     public ConfermaAlert(String text) {
-        
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Conferma");
+        alert.setHeaderText("Conferma");
+        alert.setContentText(text);
+        alert.showAndWait().ifPresent(response -> {
+            if(response == ButtonType.OK) esito = true;
+        });
+    }
+    
+    public boolean getEsito() {
+        return esito;
     }
     
 }
