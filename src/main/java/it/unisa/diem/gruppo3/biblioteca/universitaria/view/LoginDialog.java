5c371726e311161ff435801b0e775eefc8a71674
Gaetano Matteo Sirignano <>
2025-12-17 21:54:14 +0100

    Implementazione della view del dialog di Login e di Signup (creazionepassword), di nuovo

--- src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/view/LoginDialog.java
+++ src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/view/LoginDialog.java
@@ -1,10 +1,8 @@
 package it.unisa.diem.gruppo3.biblioteca.universitaria.view;
 
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.Cursor;
 import javafx.scene.control.Button;
