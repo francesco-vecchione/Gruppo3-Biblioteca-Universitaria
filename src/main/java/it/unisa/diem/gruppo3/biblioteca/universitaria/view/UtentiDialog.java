9e6fd271666c6f292ddea045f48d21724449b0b6
Gaetano Matteo Sirignano <>
2025-12-18 21:23:45 +0100

    Personalizzazione GUI di UtentiDialog

--- src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/view/UtentiDialog.java
+++ src/main/java/it/unisa/diem/gruppo3/biblioteca/universitaria/view/UtentiDialog.java
@@ -1,11 +1,22 @@
 package it.unisa.diem.gruppo3.biblioteca.universitaria.view;
 
 import it.unisa.diem.gruppo3.biblioteca.universitaria.model.Utente;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.InnerShadow;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextBoundsType;
 
 /**
  * @file UtentiDialog.java
@@ -49,32 +60,89 @@ public class UtentiDialog {
      * specifici per l'inserimento o la modifica di un utente
      */
     public UtentiDialog() {
+        
+        
+        //creazione della dialog con lo sfondo
         dialog = new Dialog<>();
-        txfNome = new TextField();
-        txfNome.setPromptText("Nome");
-        txfCognome = new TextField();
-        txfCognome.setPromptText("Cognome");
-        txfMatricola = new TextField();
-        txfMatricola.setPromptText("Matricola");
-        txfEmail = new TextField();
-        txfEmail.setPromptText("Email");
+        dialog.getDialogPane().setStyle("-fx-background-color: #FFFDF5;");
+        dialog.setTitle("Registrazione nuovo utente");
+
+        //creazione della anchorpane
+        AnchorPane root = new AnchorPane();
+        root.setPrefSize(400,50);
+        root.setStyle("-fx-background-color: #FFFDF5;");
+
+        //Text titolo (CHE NON CAMBIA DA MODIFICA A INSERIMENTO)
+        Text headerText = new Text("Inserisci i dati dell'utente");
+        headerText.setBoundsType(TextBoundsType.VISUAL);
+        headerText.setLayoutX(20.0);
+        headerText.setLayoutY(50.0);
+        headerText.setWrappingWidth(360.0);
+        headerText.setTextAlignment(TextAlignment.CENTER);
+        headerText.setFont(Font.font("Avenir Next", FontWeight.BOLD, FontPosture.ITALIC, 25));
+
+        //creazione dei textfield con la spaziatura fissa
+        double startY = 90.0;
+        double gap = 55.0;
+
+        txfNome= createCustomTextField("Nome", 60.0, startY);
+        txfCognome= createCustomTextField("Cognome", 60.0, startY + gap);
+        txfMatricola= createCustomTextField("Matricola", 60.0, startY + gap * 2);
+        txfEmail= createCustomTextField("E-Mail", 60.0, startY + gap * 3);
+
+
         
-        VBox form = new VBox();
-        form.getChildren().addAll(txfNome, txfCognome, txfMatricola, txfEmail);
-        dialog.getDialogPane().setContent(form);
-        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+        //aggiungo tutto alla anchorpane
+        root.getChildren().addAll(
+            headerText, 
+            txfNome,
+            txfCognome, 
+            txfMatricola, 
+            txfEmail
+        );
+
+        //aggiungo la anchorpane al dialogpane
+        dialog.getDialogPane().setContent(root);
+
+        //definizione del bottone della dialogpane
+        ButtonType typeEntra = new ButtonType("Conferma", ButtonBar.ButtonData.OK_DONE);
         
-        btnOk = (Button)dialog.getDialogPane().lookupButton(ButtonType.OK);
+        //aggiungo il bottone alla dialog pane
+        dialog.getDialogPane().getButtonTypes().addAll(typeEntra);
+
         
-        dialog.setTitle("Registra");
-        dialog.setHeaderText("Registra un Nuovo Utente");
+        //chiamo il bottone per personalizzarlo
+        btnOk = (Button) dialog.getDialogPane().lookupButton(typeEntra);
+
+        //effetto ombra del bottone
+        InnerShadow buttonShadow = new InnerShadow();
+        buttonShadow.setBlurType(BlurType.TWO_PASS_BOX);
+
+        //stile bottone "entra"
+        btnOk.setPrefSize(119.0, 36.0);
+        btnOk.setStyle("-fx-background-color: #6AB565; -fx-text-fill: black; -fx-background-radius:8;"); // Verde
+        btnOk.setFont(new Font("Kodchasan SemiBold Italic", 17.0));
+        btnOk.setCursor(Cursor.HAND);
+        btnOk.setOnMouseEntered(e -> btnOk.setEffect(buttonShadow));
+        btnOk.setOnMouseExited(e -> btnOk.setEffect(null));
+        btnOk.setTranslateX(-130); //sposta il tasto in basso al centro  
+    }
+    private TextField createCustomTextField(String prompt, double layoutX, double layoutY) {
+        TextField tf = new TextField();
+        tf.setPromptText(prompt);
+        tf.setLayoutX(layoutX);
+        tf.setLayoutY(layoutY);
+        tf.setPrefSize(280.0, 36.0);
+        tf.setStyle("-fx-background-radius: 10; -fx-background-color: white; -fx-border-color: #e0e0e0; -fx-border-radius: 10;"); 
+        tf.setFont(Font.font("Kodchasan", FontWeight.SEMI_BOLD, FontPosture.ITALIC, 16));
+        tf.setCursor(Cursor.TEXT);
+        return tf;
     }
     
     public UtentiDialog(Utente target) {
         this();
         
         dialog.setTitle("Modifica");
-        dialog.setHeaderText("Modifica questo Utente");
         
         txfNome.setText(target.getNome());
         txfCognome.setText(target.getCognome());
